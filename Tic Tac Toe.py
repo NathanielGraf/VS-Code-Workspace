ab30b9181c004a17090a05b546e84efc2d7c501e
NathanielGraf <nathanielthecool@gmail.com>
2022-06-01 18:45:22 -0500

    Added basic blackjack simulator to be worked on later with all the rules we want

---
+++ Tic Tac Toe.py
@@ -0,0 +1,23 @@
+import math
+import random
+
+class Player:
+    def __init__(self, letter):
+        self.letter = letter 
+        
+    def getmove(self, game):
+        pass
+
+class RandomComputerPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+        
+    def getmove(self, game):
+        pass
+    
+class HumanPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+        
+    def getmove(self, game):
+        pass
