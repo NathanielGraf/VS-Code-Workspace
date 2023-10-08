6aa68944e7c2c530c0b85bb6228808083fb0bf92
NathanielGraf <nathanielthecool@gmail.com>
2023-10-08 17:29:52 -0500

    Changed name

--- CPCodeForces/2.4.23KattisQuals/problem1.py
+++ CPCodeForces/2.4.23KattisQuals/problem1.py
@@ -1,8 +1,7 @@
 def main():
-
-    x = int(input())
-    print(x/4)
-        
-    return 0
+    inputarray = input().split()
+    inputarray = [int(i) for i in inputarray]
+    print((2-inputarray[0]) * (3-inputarray[1]) * (5-inputarray[2]) * (7-inputarray[3]) * (11-inputarray[4]) * (13-inputarray[5]) * (17-inputarray[6]) * (19-inputarray[7]) -1)
+    return
 
 main()
